d1efa9a40c850b209bf734f297b3af3a9a9b8dff
Sean Andersen <smandersen13@gmail.com>
2018-01-13 21:36:36 -0500

    updated googlemaps style and zoom

--- googlemaps.js
+++ googlemaps.js
@@ -7,8 +7,144 @@ function googleMaps() {
   //creates map in HTML centered on Washington, D.C.
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 38.9072,lng: -77.0369},
-    zoom: 11,
-    mapTypeId: 'roadmap'
+    zoom: 10,
+    gestureHandling: 'greedy',
+    styles: [
+  {
+    "elementType": "geometry",
+    "stylers": [{"color": "#f5f5f5"}]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [{"visibility": "off"}]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#616161"}]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [{"color": "#f5f5f5"}]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [{"visibility": "off"}]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#bdbdbd"}]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#5500aa"}]
+  },
+  {
+    "featureType": "administrative.neighborhood",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#a74fff"}]
+  },
+  {
+    "featureType": "landscape.man_made",
+    "elementType": "geometry.stroke",
+    "stylers": [{"color": "#5500aa"}]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [{"visibility": "on"}]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [{"color": "#eeeeee"}]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [{"visibility": "off"}]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#757575"}]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [{"color": "#e5e5e5"}]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [{"color": "#a6d89e"}]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#9e9e9e"}]
+  },
+  {
+    "featureType": "road",
+    "stylers": [{"visibility": "on"}]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{"color": "#ffffff"}]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#757575"}]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [{"color": "#dadada"}]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#616161"}]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [{"visibility": "off"}]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#9e9e9e"}]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [{"color": "#e5e5e5"}]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [{"color": "#eeeeee"}]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{"color": "#c9c9c9"}]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [{"color": "#b0d8ff"}]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [{"color": "#9e9e9e"}]
+  }
+]
   });
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
@@ -46,7 +182,7 @@ function googleMaps() {
             size: new google.maps.Size(50, 50),
             origin: new google.maps.Point(0, 0),
             anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(15, 15)
+            scaledSize: new google.maps.Size(25, 25)
           };
           // Create a marker for each place.
           var marker = new google.maps.Marker({
@@ -59,7 +195,9 @@ function googleMaps() {
           markers.push(marker);
           console.log(markers)
           //Click event to add Marker name to array to link with FourSquare
-          google.maps.event.addListener(marker, 'click', function() {
+          google.maps.event.addListener(marker, 'click', function(location) {
+            map.setZoom(18);
+            map.setCenter(location.latLng)
             markerName = marker.title
             console.log(markerName)
             /*
