dc33e84f29f2551512eca19271364c67b27b548e
Sean Andersen <smandersen13@gmail.com>
2018-01-15 19:07:46 -0500

    removed directions test

--- googlemaps.js
+++ googlemaps.js
@@ -217,80 +217,6 @@ function googleMaps() {
           });
           map.fitBounds(bounds);
         });
-        /*
-  new AutocompleteDirectionsHandler(map)
-        function AutocompleteDirectionsHandler(map) {
-          this.map = map;
-          this.originPlaceId = null;
-          this.destinationPlaceId = null;
-          this.travelMode = 'DRIVING';
-          var originInput = document.getElementById('origin-input');
-          var destinationInput = document.getElementById('destination-input');
-          this.directionsService = new google.maps.DirectionsService;
-          this.directionsDisplay = new google.maps.DirectionsRenderer;
-          this.directionsDisplay.setMap(map);
-
-          var originAutocomplete = new google.maps.places.Autocomplete(
-              originInput, {placeIdOnly: true});
-          var destinationAutocomplete = new google.maps.places.Autocomplete(
-              destinationInput, {placeIdOnly: true});
-
-          this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
-          this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
-
-          this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-          this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
-        }
-
-        // Sets a listener on a radio button to change the filter type on Places
-        // Autocomplete.
-        AutocompleteDirectionsHandler.prototype.setupClickListener = function(id, mode) {
-          var radioButton = document.getElementById(id);
-          var me = this;
-          radioButton.addEventListener('click', function() {
-            me.travelMode = mode;
-            me.route();
-          });
-        };
-
-        AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
-          var me = this;
-          autocomplete.bindTo('bounds', this.map);
-          autocomplete.addListener('place_changed', function() {
-            var place = autocomplete.getPlace();
-            if (!place.place_id) {
-              window.alert("Please select an option from the dropdown list.");
-              return;
-            }
-            if (mode === 'ORIG') {
-              me.originPlaceId = place.place_id;
-            } else {
-              me.destinationPlaceId = place.place_id;
-            }
-            me.route();
-          });
-
-        };
-
-        AutocompleteDirectionsHandler.prototype.route = function() {
-          if (!this.originPlaceId || !this.destinationPlaceId) {
-            return;
-          }
-          var me = this;
-
-          this.directionsService.route({
-            origin: {'placeId': this.originPlaceId},
-            destination: {'placeId': this.destinationPlaceId},
-            travelMode: this.travelMode
-          }, function(response, status) {
-            if (status === 'OK') {
-              me.directionsDisplay.setDirections(response);
-            } else {
-              window.alert('Directions request failed due to ' + status);
-            }
-          });
-        };
-        */
 }
 function initMap() {
           map = new google.maps.Map(document.getElementById('map'), {
@@ -301,14 +227,17 @@ function initMap() {
 
           var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
           var icons = {
-            parking: {
-              icon: iconBase + 'parking_lot_maps.png'
+            restaurant: {
+              icon: "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png"
             },
-            library: {
-              icon: iconBase + 'library_maps.png'
+            museum: {
+              icon: "https://maps.gstatic.com/mapfiles/place_api/icons/museum-71.png"
             },
-            info: {
-              icon: iconBase + 'info-i_maps.png'
+            hotel: {
+              icon: "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png"
+            }
+            bar: {
+              icon: "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png"
             }
           };
 
