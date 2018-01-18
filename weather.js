321382360693bb71e70a338050fa6309702b8357
Sean Andersen <smandersen13@gmail.com>
2018-01-17 20:01:50 -0500

    finished 24/3hour interval weather api to HTML

--- weather.js
+++ weather.js
@@ -1,63 +1,55 @@
 
+//24-Hour Weather Forecast/ 3 Hour Interval
 $("#weatherSubmit").on("click", function(){
-
+  //Clears table if the query is run again during same session
+  $("#forecastDisplay").empty()
+  //Collect City and Country from form
   var weatherCity = $("#weatherCity").val()
   var weatherCC = $("#weatherCC").val()
-
-  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + weatherCity + "," + weatherCC + "&appid=a9f634489329fd91f8011bddf4e0890e&units=Imperial&cnt=40"
+  //URL plugs in City/Country
+  var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + weatherCity + "," + weatherCC + "&appid=a9f634489329fd91f8011bddf4e0890e&units=Imperial&cnt=8"
   console.log("queryURL", queryURL)
-
+  //Ajax to reach data and provide response
   $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
           console.log(response)
-
-          var responseArray = response.list
-          //log the array to make sure everything is correct
+          //Array for 24-Hour Forecast at 3 hour Intervals
+          responseArray = response.list
+          //Log the array to make sure everything is correct
           console.log(responseArray)
           for (var i = 0; i < responseArray.length; i++) {
-
-
+            //Holds data of interest in variables
             var date = responseArray[i].dt_txt
-
             var temp = responseArray[i].main.temp
             var weatherMain = responseArray[i].weather["0"].main
             var weatherDesc = responseArray[i].weather["0"].description
             var icon = responseArray[i].weather["0"].icon
-
+            //Logs values for verification
             console.log(date, temp, weatherMain, icon)
-
-            //create containers tmrw
-            var weatherDiv = $("<div>")
-            weatherDiv.addClass('card text-white bg-primary mb-3 margin col-md-4')
-            var weatherLocationDate = $("<div>")
-            weatherLocationDate.addClass('card-header')
-            weatherLocationDate.text(weatherCity + ", " + weatherCC + " : " + date)
-            var weatherBody = $("<div>")
-            weatherBody.addClass('card-body')
-
-            var tempDiv = $("<div class='bg-secondary'>")
-            var tempDisplay = tempDiv.text(temp)
+            //Add City/Country to table
+            $("#weatherLocation").text(weatherCity + ", " + weatherCC)
+            //Variables created to hold data and append into table
+            var tBody = $("tBody")
+            var tRow = $("<tr>")
+            var weatherTD = $("<td>")
+            weatherTD.text(date)
+            var tempTD = $("<td>")
+            tempTD.text(temp + " F")
+            var weatherDescTD = $("<td>")
+            weatherDescTD.text(weatherMain + " - " + weatherDesc)
+            var weatherTimeTD = $("<td>")
             var weatherTime = $("<img>")
             var imgSrc = "https://openweathermap.org/img/w/" + icon + ".png"
             weatherTime.attr("src", imgSrc)
-            var weatherDisplay = weatherTime.text(weatherMain + " - " + weatherDesc)
-
-
-              $("#weatherDisplay").append(weatherDiv)
-              $(weatherDiv).append(weatherLocationDate)
-              $(weatherDiv).append(weatherBody)
-              $(weatherBody).append(tempDiv)
-              $(weatherBody).append(weatherTime)
-
-
-              //--------Test------//
-
-
-
+            //Add to HTML
+            weatherTimeTD.append(weatherTime)
+            tBody.append(tRow)
+            tRow.append(weatherTD)
+            tRow.append(tempTD)
+            tRow.append(weatherDescTD)
+            tRow.append(weatherTimeTD)
           }
-
       })
-
 })
