7d0d63d0d071e1e8e75cb2dd143a8e5a26646962
Sean Andersen <smandersen13@gmail.com>
2018-01-19 23:24:58 -0500

    added additional html pages

--- googlemaps.js
+++ googlemaps.js
@@ -208,7 +208,7 @@ function googleMaps() {
     ]
   });
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('map-input');
+  var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   // Bias the SearchBox results towards current map's viewport.
