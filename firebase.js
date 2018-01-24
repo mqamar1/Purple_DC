8794e96182b59771c3e9c7b817066772f3fb2485
DanYee92 <danyee92@gmail.com>
2018-01-23 19:40:34 -0500

    Feedback page and firebase

--- firebase.js
+++ firebase.js
@@ -21,21 +21,21 @@ $(".pure-button").on("click", function(event){
 
 	var name = $("#name").val();
 	var comment = $("#comment").val();
-	var email = $("#emailinput").val();
+	var rating = $("#rating_simple1").val();
 
 	console.log(name);
 	console.log(comment);
-	console.log(email);
+	console.log(rating);
 
   //push elements to firebase
   database.ref().push({
     name: name,
     comment: comment,
-    email: email,
+    rating: rating,
   });
-	retrieveData();
-
-
+  //Removed input info
+		$('#name').val('');
+		$('#comment').val('');
 
 });
 
@@ -44,21 +44,51 @@ function retrieveData(){
   database.ref().on("child_added", function(childSnapshot){
     var sv = childSnapshot.val()
     console.log(sv);
-    renderRows(sv)
   })
 }
 
-
-//function to render new rows
-function renderRows(sv){
-  var tBody = $("#userTable")
-  var tRow2 = $("<tr>")
-
-  var nameTd = $("<td>").text(sv.name);
-  var emailTd = $("<td>").text(sv.email);
-  var commentTD = $("<td>").text(sv.comment)
+database.ref().on('child_added',function render (snap){
+					//Testing
+					var nameTd = snap.val().name;
+					var ratingTd = snap.val().rating;
+					var commentTD = snap.val().comment;
 
 
-  tRow2.append(nameTd, emailTd, commentTD);
-  tBody.append(tRow2);
-}
+					//Data array
+					var dataArray = [nameTd, ratingTd, commentTD];
+					var newTr = $('<tr>');
+					for(var i = 0; i< dataArray.length; i++){
+						var newTd = $('<td>');
+						newTd.text(dataArray[i]);
+						newTd.appendTo(newTr);
+					}
+					$('.table').prepend(newTr);
+        })
+
+        $(function() {
+
+          // Initialize form validation on the registration form.
+          // It has the name attribute "registration"
+          $("form[name='feedbackForm']").validate({
+            // Specify validation rules
+            rules: {
+              // The key name on the left side is the name attribute
+              // of an input field. Validation rules are defined
+              // on the right side
+              name: "required",
+              comment: "required"
+            },
+
+            // Specify validation error messages
+            messages: {
+              name: "Please enter your full name",
+              comment: "Please provide your feedback"
+              },
+
+            // Make sure the form is submitted to the destination defined
+            // in the "action" attribute of the form when valid
+            submitHandler: function(form) {
+              form.submit();
+            }
+          });
+        });
