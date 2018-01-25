d6803d187d3d5c00f19b5787dd9baf76b416d61e
DanYee92 <danyee92@gmail.com>
2018-01-24 22:06:23 -0500

    CSS, more validations, appending order

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function() {
 
 // Initialize Firebase
 var config = {
@@ -23,72 +23,49 @@ $(".pure-button").on("click", function(event){
 	var comment = $("#comment").val();
 	var rating = $("#rating_simple1").val();
 
+  validateForm(name, comment, rating)
+
 	console.log(name);
 	console.log(comment);
 	console.log(rating);
-
-  //push elements to firebase
-  database.ref().push({
-    name: name,
-    comment: comment,
-    rating: rating,
-  });
-  //Removed input info
-		$('#name').val('');
-		$('#comment').val('');
 
 });
 
 //function to retrieve data
 function retrieveData(){
-  database.ref().on("child_added", function(childSnapshot){
-    var sv = childSnapshot.val()
-    console.log(sv);
-  })
+  database.ref().on('child_added',function (snapshot){
+      var tBody = $("#userTable")
+      var tRow = $("<tr>")
+
+      var childSnapShot = snapshot.val()
+      //Testing
+      var nameTd = $("<td class='tdform'>").text(childSnapShot.name)
+      var ratingTd = $("<td class='tdform'>").text(childSnapShot.rating)
+      var commentTD = $("<td class='tdform'>").text(childSnapShot.comment)
+
+      tRow.append(nameTd,ratingTd,commentTD)
+      tBody.prepend(tRow)
+
+    })
 }
 
-database.ref().on('child_added',function render (snap){
-					//Testing
-					var nameTd = snap.val().name;
-					var ratingTd = snap.val().rating;
-					var commentTD = snap.val().comment;
-
-
-					//Data array
-					var dataArray = [nameTd, ratingTd, commentTD];
-					var newTr = $('<tr>');
-					for(var i = 0; i< dataArray.length; i++){
-						var newTd = $('<td>');
-						newTd.text(dataArray[i]);
-						newTd.appendTo(newTr);
-					}
-					$('.table').prepend(newTr);
-        })
-
-        $(function() {
-
-          // Initialize form validation on the registration form.
-          // It has the name attribute "registration"
-          $("form[name='feedbackForm']").validate({
-            // Specify validation rules
-            rules: {
-              // The key name on the left side is the name attribute
-              // of an input field. Validation rules are defined
-              // on the right side
-              name: "required",
-              comment: "required"
-            },
-
-            // Specify validation error messages
-            messages: {
-              name: "Please enter your full name",
-              comment: "Please provide your feedback"
-              },
-
-            // Make sure the form is submitted to the destination defined
-            // in the "action" attribute of the form when valid
-            submitHandler: function(form) {
-              form.submit();
-            }
+function validateForm(name, comment, rating) {
+    if (name === "" || comment === "" || rating === "0") {
+        $("#submitFeedback").text("Please Answer All Fields")
+        jQuery.noConflict();
+        $("#feedbackModal").modal()
+        return false;
+    } else {
+        database.ref().push({
+          name: name,
+          comment: comment,
+          rating: rating,
           });
-        });
+        }
+        $('#name').val('');
+        $('#comment').val('');
+      }
+
+retrieveData()
+
+})
